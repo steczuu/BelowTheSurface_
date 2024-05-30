bcc9d57449d845cb5b0641e2972c56b916e03e77
Jacek Grzybek <gg1491625@gmail.com>
2024-05-30 22:57:52 +0200

    added basic graphics. Sry for mess in scene, i dont know how to make a folder here

--- Assets/scripts/Player Control/camera.cs
+++ Assets/scripts/Player Control/camera.cs
@@ -13,6 +13,6 @@ public class camera : MonoBehaviour
 
     void Update()
     {
-        transform.position = Vector3.Lerp(transform.position, player.position + new Vector3(0,0,-10), smoothSpeed); //smooyhly follows the player with offset of -10 (as it was by default) in z plane
+        transform.position = Vector3.Lerp(transform.position, player.position + new Vector3(0,0,-10), smoothSpeed); //smoothly follows the player with offset of -10 (as it was by default) in z plane
     }
 }
