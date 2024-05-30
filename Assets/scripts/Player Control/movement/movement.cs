89597e8b4b15d39b46f4ccbf761fe85a679102f2
Jacek Grzybek <gg1491625@gmail.com>
2024-05-30 15:40:14 +0200

    added verry basic movement

---
+++ Assets/scripts/Player Control/movement/movement.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+    float MovementSpeed = 3f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+            transform.position = transform.position + new Vector3(0, MovementSpeed * Time.deltaTime,0);
+        if (Input.GetKey(KeyCode.S))
+            transform.position = transform.position + new Vector3(0, -MovementSpeed * Time.deltaTime, 0);
+        if (Input.GetKey(KeyCode.A))
+            transform.position = transform.position + new Vector3(-MovementSpeed * Time.deltaTime,0, 0);
+        if (Input.GetKey(KeyCode.D))
+            transform.position = transform.position + new Vector3(MovementSpeed * Time.deltaTime,0, 0);
+    }
+}
