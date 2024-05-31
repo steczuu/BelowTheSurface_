6a6320f8bcfdd133838cc31bb9aed1a6ecdbfde3
Jacek Grzybek <gg1491625@gmail.com>
2024-05-31 18:35:49 +0200

    added dynamic swimming. 2 bugs spotted - truncation error of velocity, meaning player never ACTUALLY stops and movement is jitteery at higher speeds

--- Assets/scripts/Player Control/camera.cs
+++ Assets/scripts/Player Control/camera.cs
@@ -1,18 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class camera : MonoBehaviour
 {
-    public float smoothSpeed = 0.005f;
-    public Transform player;
+    public float smoothSpeed = 0.015f;
+    public GameObject player;
+    Vector3 offset = new Vector3(0, 0, -10);
     void Start()
     {
-        
+
     }
 
     void Update()
     {
-        transform.position = Vector3.Lerp(transform.position, player.position + new Vector3(0,0,-10), smoothSpeed); //smoothly follows the player with offset of -10 (as it was by default) in z plane
+        transform.position = Vector3.Lerp(transform.position, player.transform.position + offset, smoothSpeed); //smoothly follows the player with offset of -10 (as it was by default) in z plane
     }
 }
--- Assets/scripts/Player Control/movement/movement.cs
+++ Assets/scripts/Player Control/movement/movement.cs
@@ -4,40 +4,61 @@ using UnityEngine;
 
 public class movement : MonoBehaviour
 {
-    float damping = 0.02f;                   // ResistanceOfMedium
+    float damping = 0.01f;                   // ResistanceOfMedium
     float acceleration = 0.02f;              // Having lower a than damping will stop movement, also if a wont be divisble by damping value, it will cause a bug likely
-    float MaxSpeed = 5f;
-    Vector2 Velocity = new Vector2(0f,0f);  //first vertical, second horizontal
+    float MaxSpeed = 2f;                     
+    public Vector2 Velocity = new Vector2(0f,0f);   //first vertical, second horizontal
+    int i = 0;                               //used for dynamic swimming
+    int CycleDuration = 500;
+    public bool moving = false;
     void Start()
     {
         
     }
 
-    void Update()
-    {
-        if (Input.GetKey(KeyCode.W))
-            if (Velocity[0] < MaxSpeed) Velocity[0] += 0.01f;      //accelerating
-        if (Input.GetKey(KeyCode.S))
-            if (Velocity[0] < MaxSpeed) Velocity[0] -= 0.01f;      //accelerating
-        if (Input.GetKey(KeyCode.A))
-            if (Velocity[1] < MaxSpeed) Velocity[1] -= 0.01f;      //accelerating
-        if (Input.GetKey(KeyCode.D))
-            if (Velocity[1] < MaxSpeed) Velocity[1] += 0.01f;      //accelerating
+    void Update(){
+        if (Input.GetKeyDown(KeyCode.LeftShift)) {MaxSpeed = 5f; CycleDuration = 300; } //sprint
+        if (Input.GetKeyUp(KeyCode.LeftShift)) {MaxSpeed = 2f; CycleDuration = 500; }
 
-        if (!(Input.GetKey(KeyCode.W) | Input.GetKey(KeyCode.A) | Input.GetKey(KeyCode.S) | Input.GetKey(KeyCode.D))){
+        if (i > CycleDuration) i = 0;        //cycle timer
+        if (i <= 100) {
+            if (Input.GetKey(KeyCode.W)) {
+                if (Velocity[0] < MaxSpeed) Velocity[0] += acceleration;      //accelerating
+                moving = true;
+            }
+            if (Input.GetKey(KeyCode.S))
+            {
+                if (Velocity[0] < MaxSpeed) Velocity[0] -= acceleration;      //accelerating
+                moving = true;
+            }
+            if (Input.GetKey(KeyCode.A)) { 
+                if (Velocity[1] < MaxSpeed) Velocity[1] -= acceleration;      //accelerating
+                moving = true;
+            }
+            if (Input.GetKey(KeyCode.D)) { 
+                if (Velocity[1] < MaxSpeed) Velocity[1] += acceleration;      //accelerating
+                moving = true;
+            }
+        }
+
+        //if ((Velocity[0] < 0.001f) && (Velocity[0] > -0.001f)) { Velocity[0] = 0f; Debug.Log("Correcting"); }
+
+        if (!(moving)|i>100){
             if (Velocity[0] != 0){              //decelerating
                 if (Velocity[0] > 0)        Velocity[0] -= damping;
                 else                        Velocity[0] += damping;
+                i++;
             }
             if (Velocity[1] != 0){             //decelerating
                 if (Velocity[1] > 0)        Velocity[1] -= damping;
                 else                        Velocity[1] += damping;
+                i++;
             }
         }
-
-                                                //moving
-        transform.position = transform.position + new Vector3(0, Velocity[0] * Time.deltaTime, 0);
-        transform.position = transform.position + new Vector3(Velocity[1] * Time.deltaTime, 0, 0);
-
+        else i++;
+                                                                 //moving
+        transform.position = transform.position + new Vector3(Velocity[1] * Time.deltaTime, Velocity[0] * Time.deltaTime, 0);
+        //Debug.Log(Velocity[1]);
+        //Debug.Log(Velocity[0]);
     }
 }
